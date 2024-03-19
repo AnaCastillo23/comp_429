97c2113995388d315ed8538ff14a6590d1d5d3f3
AnaCastillo23 <89889778+AnaCastillo23@users.noreply.github.com>
2024-03-18 21:15:01 -0700

    Started implementing server socket

--- src/chat.java
+++ src/chat.java
@@ -1,5 +1,8 @@
-//Importing tools
+//  IMPORTING PROJECT TOOLS
+import java.io.*;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -7,7 +10,46 @@ import java.util.Scanner;
 public class chat {
     public static void main(String[] args) {
 
-        //Scanner to get user input
+
+        //  CREATE SERVER SOCKET (DOOR)
+        /* To welcome client's contact*/
+        final ServerSocket serverSocket;
+
+        //  CLIENT CONTACTS SERVER BY:
+        //  CREATE TCP CLIENT SOCKET
+        /* Specify the IP address and port number of server process.
+        * When created, client TCP establishes connection to server TCP.*/
+        final Socket clientSocket;
+
+        //  TO READ DATA FROM THE clientSocket
+        final BufferedReader in;
+        //  TO WRITE DATA INTO THE clientSocket
+        final PrintWriter out;
+
+
+        //  INITIATE serverSocket
+        try {
+            serverSocket = new ServerSocket(29001);
+            clientSocket = serverSocket.accept();
+            //  TO WRITE TO THE clientSocket
+            out = new PrintWriter(clientSocket.getOutputStream());
+            //  TO READ FROM THE clientSocket
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //INITIATE clientSocket
+
+
+
+
+
+
+
+
+
+        //  SCANNER TO GET USER INPUT
         Scanner scanner_for_help = new Scanner(System.in);
         System.out.println("\n*************WELCOME TO iCHAT**************\n");
         System.out.println("Type 'help' for information about the available user interface options.");
@@ -17,6 +59,7 @@ public class chat {
 
         String help_string_holder = scanner_for_help.nextLine();
 
+        //  USER MENU WITH ERROR CHECKING
         try {
             if (help_string_holder.equals("help")) {
 
@@ -35,10 +78,7 @@ public class chat {
                     System.out.println("7) Exit: Close all connections and terminate this process");
                     System.out.println("\nPlease enter the number of your choice: ");
 
-
                     choice = input.nextInt();
-                    //WE STILL NEED TO IMPLEMENT ERROR CHECKING FOR choice TO MAKE SURE USER DOES NOT INPUT A STRING
-
 
                     switch (choice) {
                         case 1:
@@ -48,7 +88,10 @@ public class chat {
                             System.out.println("The IP address is " + myIP.getHostAddress());
                             break;
                         case 2:
-                            System.out.println("\nmyPort");
+                            //  TO PRINT THE PASSED PORT NUMBER FROM THE COMMAND LINE ARGUMENT
+                            /* DELETE THIS COMMENT LATER->THIS SECTION ONLY RUNS IN THE COMMAND PROMPT, NOT IN HERE AS WE NEED TO PASS IN A COMMAND LINE ARGUMENT
+                              WHEN RUNNING PROGRAM */
+                            System.out.println("\nmyPort:");
                             System.out.println("The program runs on port number " + args[0]);
                             break;
                         case 3:
@@ -78,11 +121,12 @@ public class chat {
                 System.out.println("Have a good day!");
                 System.exit(0);
             } else {
+                //  CLOSING PROGRAM WHEN USER TYPES ANOTHER THING OTHER THAN help OR exit
                 System.out.println("Command not found. Please try again.\n");
                 System.exit(0);
             }
         } catch (InputMismatchException e) {
-            // TO MAKE SURE USER HAS INPUTTED THE RIGHT TYPE OF INPUT
+            //  HANDLING USER INPUT WHEN AN EXCEPTION OCCURS (USER TYPE INCORRECT INPUT TYPE)
             System.out.print("Command not found. Please try again.\n");
             System.exit(0);
         } catch (UnknownHostException e) {
